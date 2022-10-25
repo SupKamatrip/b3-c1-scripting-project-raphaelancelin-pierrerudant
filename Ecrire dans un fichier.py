0380931d8ed706eb4bcb69a1ab62d46a80168935
Pierre RUDANT <pierrerudant@gmail.com>
2022-10-25 10:19:16 +0200

    Correction Tuto Ecrire dans un fichier

--- Ecrire dans un fichier.py
+++ Ecrire dans un fichier.py
@@ -1,5 +1,5 @@
 import csv
-with open('conso-annuelles_v1.csv', newline='') as csvfile:
+with open('conso-annuelles_v1.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
     spamwriter.writerow(['Spam', 'Lovely Spam'])
