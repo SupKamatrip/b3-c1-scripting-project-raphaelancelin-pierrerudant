e9124ee05f5b2fcdb8a05608e88aa6da7436ed6b
Pierre RUDANT <pierrerudant@gmail.com>
2022-10-25 12:29:51 +0200

    V1 Script - Print list

--- script.py
+++ script.py
@@ -1,10 +1,14 @@
-import csv
-with open('conso-annuelles_v1.csv', newline='') as f:
-    tableau=[]
-    lire=csv.reader(f)
-    print('', end= '\n')
-    print('Affichage des lignes du tableau', end='\n')
-    for ligne in lire:
-        print(ligne, end='\n')
-        tableau.append(ligne)
-        
+with open('conso-annuelles_v1.csv') as file:
+    content = file.readlines()
+    header = content[:1]
+    rows = content[1:]
+
+# print(header)
+# print(rows[5])
+# print("\n")
+print(rows[1])
+
+
+for x in range (len(rows)):
+    print (rows[x:])
+
