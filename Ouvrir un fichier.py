dfa5f047569f5804e94bded55fac9581922035f2
SupKamatrip <raphael.ancelin@epsi.fr>
2022-11-28 11:05:33 +0100

    push

--- Ouvrir un fichier.py
+++ Ouvrir un fichier.py
@@ -1,10 +1,6 @@
-import csv
-with open('conso-annuelles_v1.csv', newline='') as f:
-    tableau=[]
-    lire=csv.reader(f)
-    print('', end= '\n')
-    print('Affichage des lignes du tableau', end='\n')
-    for ligne in lire:
-        print(ligne, end='\n')
-        tableau.append(ligne)
-        
+import csv;
+
+f= open(r"conso-annuelles_original.csv")
+myReader = csv.reader(f)
+for row in myReader:
+    print(row)
