26ba90cd1f10c867186057cf212e1b2f685aa59f
SupKamatrip <raphael.ancelin@epsi.fr>
2022-11-29 09:55:56 +0100

    push

--- Ouvrir un fichier.py
+++ Ouvrir un fichier.py
@@ -8,4 +8,4 @@ for row in myReader:
 
     #print(str.strip(row[0])) # str.strip retire les espaces
     #print(str.strip(row[2]) + str.strip(row[3])) # additionne les colonnes 2 et 3
-    #print(str.strip(row[4]))
+    #print(str.strip(row[4])) #
