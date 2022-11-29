f8129b9943b9ab07d3696059a5b33f7f04eaf2a9
SupKamatrip <raphael.ancelin@epsi.fr>
2022-11-29 11:23:35 +0100

    push

--- Ecrire dans un fichier.py
+++ Ecrire dans un fichier.py
@@ -1,14 +1,22 @@
 import csv
-x=[]
-y=[]
-t=[]
-with open('conso-annuelles_original.csv', newline='') as f:
-    reader = csv.reader(f, delimiter=';')
-    for col in reader:
-        t.append(str(col[0].replace(",", ".")))
-        x.append(str(col[2].replace(",", "."))) #n°1 = 2eme colonne
-        y.append(str(col[3].replace(",", "."))) #n°2 = 3eme colonne
 
-print(t)
-print(x)
-print(y)
+s = open(r"conso-annuelles_original.csv")
+f = open(r"conso-annuelles_v1.csv")
+myReader = csv.reader(f, delimiter=';')
+myWriter = csv.writer(s, delimiter=';')
+
+for row in myReader:
+    print(str.strip(row[0])) # str.strip retire les espaces
+    print(str.strip(row[2]) + str.strip(row[3])) # additionne les colonnes 2 et 3
+    print(str.strip(row[4])) #
+
+
+#with open("conso-annuelles_original.csv",'r') as file:
+#    data = file
+#file_csv = 'Data2.csv'
+#csv_file = open('conso-annuelles_original.csv', 'r')
+#with open('conso-annuelles_v1.csv', 'a') as writable:
+ #   writer = csv.writer(writable)
+  #  for row in csv_file:
+   #     writer.writerow(str.strip(row[0]))
+
