4aaa32d7488d644db1fdde0ad94c6df66c15b1c4
SupKamatrip <raphael.ancelin@epsi.fr>
2022-11-29 09:52:20 +0100

    push

--- Ecrire dans un fichier.py
+++ Ecrire dans un fichier.py
@@ -1,5 +1,14 @@
 import csv
-with open('conso-annuelles_v1.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-    spamwriter.writerow(['Spam', 'Lovely Spam'])
+x=[]
+y=[]
+t=[]
+with open('conso-annuelles_original.csv', newline='') as f:
+    reader = csv.reader(f, delimiter=';')
+    for col in reader:
+        t.append(str(col[0].replace(",", ".")))
+        x.append(str(col[2].replace(",", "."))) #n°1 = 2eme colonne
+        y.append(str(col[3].replace(",", "."))) #n°2 = 3eme colonne
+
+print(t)
+print(x)
+print(y)
