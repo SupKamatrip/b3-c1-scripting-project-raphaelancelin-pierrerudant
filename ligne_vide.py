1cf250ebdd92d9b43e124170e35c6345ff51291e
PRudant <116551948+PRudant@users.noreply.github.com>
2022-12-03 14:37:25 +0100

    Update V2
    Supprime les lignes vide
    Additionne AN1 + AN2
    + Créer un csv propre

--- ligne_vide.py
+++ ligne_vide.py
@@ -1,16 +1,44 @@
 import csv
 f = open(r"conso-annuelles_original.csv")
+s = open("conso-annuelles_v2.csv", "w")
 myReader = csv.reader(f, delimiter=';')
+myWriter = csv.writer(s, delimiter=';')
 x = 0
+v = 0
+p = 0
 
 for row in myReader:
     x = x + 1
     if not all(row):
-        print(x, "vide")
+        #v = v + 1 #ligne vide
+        print(x, v, "vide")
     else:
-        print(x, "non")
+        #p = p + 1 #ligne bonne
+        print(x, p, "non")
 
-    #print(row[0]) #Lis la colonne 0
+
+        if row[0] != 'Appareil suivi':
+            if row[2] == '-':
+                AN2 = row[2].replace('-', '0')
+
+            else:
+                AN1 = row[2]
+
+
+            if row[3] == '-':
+                AN2 = row[3].replace('-', '0')
+
+            else:
+                AN2 = row[3]
+
+
+            consototal = float(AN1.replace(',', '.')) + float(AN2.replace(',', '.'))
+
+
+            writable = row[0], consototal
+            myWriter.writerow(writable)
+
+    #print(row[0]) #Lis la colonne 0 
 
     #print(row) # Lis le tableur ligne par ligne
 
