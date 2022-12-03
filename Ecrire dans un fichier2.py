5fed043d95ee90cb1d523ecdbdf0aca3f5652172
SupKamatrip <raphael.ancelin@epsi.fr>
2022-12-03 16:27:16 +0100

    sdwfbgdq

--- Ecrire dans un fichier2.py
+++ Ecrire dans un fichier2.py
@@ -1,10 +1,9 @@
-import csv
+import csv, operator
+
 f = open(r"conso-annuelles_v1.csv")
 s = open("conso_annuelles_v2.csv",'w')
 myReader = csv.reader(f, delimiter=';')
 f = ''.join([i for i in f])
-f = f.replace('ID logement','CT Annuelle')
-for row in f:
-    print(row)
-#s = sorted(f, reverse=True)
-#print(f)
+f = f.replace('ID logement','CT Annuelle')  # remplacement de la colonne id logement par la consommation totale
+f = sorted(f, key=lambda x: [1])
+print(f)
--- ligne_vide.py
+++ ligne_vide.py
@@ -1,4 +1,4 @@
-import csv
+import csv, operator
 f = open(r"conso-annuelles_original.csv")
 s = open("conso-annuelles_v2.csv", "w")
 myReader = csv.reader(f, delimiter=';')
@@ -10,33 +10,31 @@ p = 0
 for row in myReader:
     x = x + 1
     if not all(row):
-        #v = v + 1 #ligne vide
+        v = v + 1 #ligne vide
         print(x, v, "vide")
     else:
-        #p = p + 1 #ligne bonne
+        p = p + 1 #ligne bonne
         print(x, p, "non")
-
 
         if row[0] != 'Appareil suivi':
             if row[2] == '-':
                 AN2 = row[2].replace('-', '0')
-
             else:
                 AN1 = row[2]
 
 
             if row[3] == '-':
                 AN2 = row[3].replace('-', '0')
-
             else:
                 AN2 = row[3]
-
-
             consototal = float(AN1.replace(',', '.')) + float(AN2.replace(',', '.'))
-
-
-            writable = row[0], consototal
+            writable = row[0], consototal, row[4]
             myWriter.writerow(writable)
+g = open("conso-annuelles_v2.csv", "r") # on reouvre le fichier qu'on  a modifié
+t = open("conso-annuelles_v3.csv", "w") #
+consoclean = sorted(g, key=lambda x: (x[2],x[1]), reverse=True)
+print(g)
+#print(f)
 
     #print(row[0]) #Lis la colonne 0 
 
